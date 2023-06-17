d778996f71033136cf983a8263e89ede1391eec1
Nhan Vo <vtnhanhcmus@gmail.com>
2023-06-17 14:12:13 +0800

    init service eareka server

---
+++ eurekaserver/src/test/java/com/carpark/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpark.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
