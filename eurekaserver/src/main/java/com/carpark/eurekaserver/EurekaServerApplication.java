5942a98fb18d9f4e62d73ff32a68f1df3981d17f
Nhan Vo <vtnhanhcmus@gmail.com>
2023-06-17 14:23:55 +0800

    enable eureka server and config eureka read properties from server config service

--- eurekaserver/src/main/java/com/carpark/eurekaserver/EurekaServerApplication.java
+++ eurekaserver/src/main/java/com/carpark/eurekaserver/EurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.carpark.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaserverApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
