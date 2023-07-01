e6fdae628af8a43bbafda4af5f5f161abee2d906
Nhan Vo <vtnhanhcmus@gmail.com>
2023-07-01 18:46:49 +0800

    add resilience4j for booking service

--- bookings/src/main/java/com/carpark/bookings/controllers/BookingController.java
+++ bookings/src/main/java/com/carpark/bookings/controllers/BookingController.java
@@ -4,10 +4,15 @@ import com.carpark.bookings.config.BookingProperties;
 import com.carpark.bookings.dtos.BookingDetailDto;
 import com.carpark.bookings.dtos.BookingDto;
 import com.carpark.bookings.services.BookingService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @Slf4j
@@ -25,8 +30,16 @@ public class BookingController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<BookingDetailDto> createBooking(@RequestBody BookingDto bookingRequest){
-        return ResponseEntity.ok(bookingService.createBooking(bookingRequest));
+    @CircuitBreaker(name = "booking", fallbackMethod = "bookingFallBackMethod")
+    @TimeLimiter(name = "booking")
+    @Retry(name = "booking")
+    public CompletableFuture<BookingDetailDto> createBooking(@RequestBody BookingDto bookingRequest){
+        return CompletableFuture.supplyAsync(() -> bookingService.createBooking(bookingRequest));
+    }
+
+    public CompletableFuture<String> bookingFallBackMethod(BookingDto bookingRequest){
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Something went wrong with your booking with car park info " + bookingRequest.getCarParkNo());
     }
 
     @GetMapping("/config")
